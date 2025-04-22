f62b384ab743edd5e8dfa6e3658c38fd93099908
EnochPei <enochpeienochpei@gmail.com>
2025-04-22 12:56:01 +1000

    Initial Commit

---
+++ Assignment 3/Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_3App.swift
+//  Assignment 3
+//
+//  Created by 張宇漢 on 22/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment 3/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Assignment 3
+//
+//  Created by 張宇漢 on 22/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            // a
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
