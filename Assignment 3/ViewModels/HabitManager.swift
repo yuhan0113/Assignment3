012c6bde1bef03ee6d05388563d2f86dcde7ffe8
yuhan0113 <2856528583@qq.com>
2025-05-09 23:20:38 +1000

    little updates
    - removed 1 line in habitviewmodel.swift

--- Assignment 3/ViewModels/HabitManager.swift
+++ Assignment 3/ViewModels/HabitManager.swift
@@ -3,7 +3,7 @@ import Foundation
 class HabitManager: ObservableObject {
     @Published var habits: [Habit] = []
     @Published var logs: [UUID: [HabitLog]] = [:]
-    // Habit ID → Logs
+    // Habit ID -> Logs
 
     // You can include add/delete habit, and log tracking here if needed
 }
